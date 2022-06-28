2d586cf8c1420ff068204749545d8a19342226ab
Kosh <mail@ex.com>
2022-06-28 20:20:23 +0300

    Done task10'

---
+++ Task10/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.Write("Введите трёхзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number / 100 > 10 || number / 100 == 0)
+{
+    Console.Write("Вы ввели неправильное число. Введите трёхзначное число: ");
+    int numb = Convert.ToInt32(Console.ReadLine());
+    number = numb;
+}
+
+int Decim(int decim)
+{
+    return (decim / 10) % 10;
+}
+
+Console.WriteLine(Decim(number));
